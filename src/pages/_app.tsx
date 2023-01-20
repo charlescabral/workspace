1703227644b41c64d3d9af29d4ab6fcddaf16c9e
Charles <charles@frontlab-workstation.local>
2023-01-19 21:23:49 -0300

    add head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Charles Cabral</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
+        <meta name="description" content="A simple to show my job" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
