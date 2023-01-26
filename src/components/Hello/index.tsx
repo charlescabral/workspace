5d8efec6734ba5a88c933e52d4286f82865572f4
charlescabral <charles.cabral.neto@gmail.com>
2023-01-26 18:55:23 -0300

    add pwa

---
+++ src/components/Hello/index.tsx
@@ -0,0 +1,7 @@
+const Hello = () => (
+  <main>
+    <h1>Olar</h1>
+  </main>
+)
+
+export default Hello
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import HomeTemplate from '@/templates/Home'
 
-const asddasd = asdasd
-
 export default function Home() {
   return <HomeTemplate />
 }
