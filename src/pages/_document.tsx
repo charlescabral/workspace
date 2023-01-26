3d5d2e381e7bdb98c976854bec96bcfa4309c504
Charles <charles@frontlab-workstation.local>
2023-01-26 09:44:20 -0300

    add home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText, reset } from '@/styles'
+
+const getCssAndReset = () => {
+  const css = getCssText()
+  reset()
+  return css
+}
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssAndReset() }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import HomeTemplate from '@/templates/Home'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <h1>Charles Cabral</h1>
-          <p>Front-end Developer</p>
-          <a href="mailto:charles.cabral.neto@gmail.com">charles.cabral.neto@gmail.com</a>
-        </div>
-      </main>
-    </>
-  )
+  
+  return <HomeTemplate />
 }
